e1b98d10dd8d4651ab528c39e5376732c1474ac5
Administrador <1234>
2015-02-12 15:47:58 -0500

    Add log and toast, as well as onClick for some buttons

--- app/src/main/java/com/augustosalazar/calculadoralayoutdemo/MainActivity.java
+++ app/src/main/java/com/augustosalazar/calculadoralayoutdemo/MainActivity.java
@@ -2,11 +2,18 @@ package com.augustosalazar.calculadoralayoutdemo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +43,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void WriteNumber(View view) {
+
+        Button b = (Button) findViewById(view.getId());
+        EditText ed = (EditText) findViewById(R.id.editText);
+
+        ed.setText(ed.getText().toString() + b.getText());
+
+        Log.d(TAG, (String) b.getText());
+
+        Toast.makeText(this,(String) b.getText(),Toast.LENGTH_SHORT).show();
+
     }
 }
